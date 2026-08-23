b5fc260df4929f0418e40a52e7a0e88bf1ed09d9
Mth71 <mthrocha07@outlook.com>
2026-08-23 13:24:40 -0300

    Add placeholder do modo workflow

---
+++ include/workflow.h
@@ -0,0 +1,6 @@
+#ifndef WORKFLOW_H
+#define WORKFLOW_H
+
+void run_workflow(const char *filename);
+
+#endif 
---
+++ src/workflow.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "workflow.h"
+
+void run_workflow(const char *filename){
+    printf("(ainda n implementada) executaria um arquivo %s\n", filename);
+}

d2ab3318fafd4b25b8bceabd3b3e0ff6a897aad0
Mth71 <mthrocha07@outlook.com>
2026-08-23 18:23:00 -0300

    chore: Integra 'tokenize' ao loop interativo.

--- src/interactive.c
+++ src/interactive.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 #include "interactive.h"
 #include "common.h"
-#include <string.h>
+#include "parser.h"
+
+#define MAX_TOKENS 64
 
 void run_interactive(void){
     char linha[MAX_LINE];
+    char *tokens[MAX_TOKENS];
 
     while(1){
         printf("processflow> ");
@@ -17,10 +21,16 @@ void run_interactive(void){
 
         linha[strcspn(linha, "\n")] = '\0';
 
+        int n = tokenize(linha, tokens, MAX_TOKENS);
+
+        if(n == 0){
+            continue;
+        }
+
         if(strcmp(linha, "exit") == 0){
             break;
         }
 
-        printf("Você digitou: %s", linha);
+        printf("Comando aceito: '%s' (com %d token(s) no total)\n", tokens[0], n);
     }
 }
